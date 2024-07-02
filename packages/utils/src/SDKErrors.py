9b3e5e22e0f446049a4db78d806978884881f1f3
Hardik Sharma <hardik182002@gmail.com>
2024-07-02 19:08:25 +0530

    written did.utils.py

--- packages/utils/src/SDKErrors.py
+++ packages/utils/src/SDKErrors.py
@@ -4,57 +4,81 @@ class SDKError(Exception):
         self.name = self.__class__.__name__
         self.options = options
 
-#config
+
+# config
 class BlockchainApiMissingError(SDKError):
     def __init__(self, options: dict = None):
         message = 'Blockchain API is missing. Please set the "api" configuration.'
         super().__init__(message, options)
 
-#network
+
+# network
 class SubscriptionsNotSupportedError(SDKError):
     def __init__(self, options: dict = None):
         message = (
-            'This function is not available if the blockchain API does not support state or event subscriptions, '
-            'use `WsProvider` to enable the complete feature set'
+            "This function is not available if the blockchain API does not support state or event subscriptions, "
+            "use `WsProvider` to enable the complete feature set"
         )
         super().__init__(message, options)
 
+
 class TimeoutError(SDKError):
     def __init__(self, options: dict = None):
-        message = 'Promise timed out'
+        message = "Promise timed out"
         super().__init__(message, options)
 
-#identifier errors
+
+# identifier errors
 class InvalidURIError(SDKError):
     pass
+
 
 class InvalidIdentifierError(SDKError):
     pass
 
+
 class InvalidInputError(SDKError):
     pass
 
-#DID errors
+
+# DID errors
 class InvalidDidFormatError(SDKError):
     def __init__(self, did: str, options: dict = None):
         message = f'Not a valid CORD DID "{did}"'
         super().__init__(message, options)
 
+
 class DidError(SDKError):
     pass
 
+
 class AddressInvalidError(SDKError):
-    def __init__(self, id,  options: dict = None):
+    def __init__(self, id=None, type=None, options: dict = None):
         if id and type:
             message = f'Provided {type} identifier "{id}" is invalid'
         elif id:
             message = f'Provided identifier "{id}" is invalid'
         else:
-            message = 'Provided identifier is invalid'
+            message = "Provided identifier is invalid"
         super().__init__(message, options)
+
 
 class AddressTypeError(SDKError):
     pass
+
+
+# utils
+class HashMalformedError(SDKError):
+    def __init__(self, hash: str = None, type: str = None, options: dict = None):
+        if hash and type:
+            message = f'Provided {type} hash "{hash}" is invalid or malformed'
+        elif hash:
+            message = f'Provided hash "{hash}" is invalid or malformed'
+        else:
+            message = "Provided hash is invalid or malformed"
+        super().__init__(message, options)
+
+
 class Errors:
     SDKError = SDKError
     SubscriptionsNotSupportedError = SubscriptionsNotSupportedError
