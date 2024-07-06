91797514a70b04d27f7c07acbe7fa31e20163ac5
Hardik Sharma <hardik182002@gmail.com>
2024-07-06 19:04:18 +0530

    demo for registrar

--- demo/src/utils/create_account.py
+++ demo/src/utils/create_account.py
@@ -1,12 +1,14 @@
 import packages.sdk.src as Cord
 
-def create_account(mnemonic = Cord.Utils.crypto_utils.generate_mnemonic()):
+def create_account(mnemonic=None):
     """
     `createAccount` creates a new account from a mnemonic
     :param mnenonic: The mnemonic phrase to use to generate the account. If not provided, a new mnemonic will be generated.
     :return: An object with two properties: account and mnemonic.
     """
+    if mnemonic is None:
+        mnemonic = Cord.Utils.crypto_utils.generate_mnemonic(24)
     return {
-        'account': Cord.Utils.crypto_utils.create_from_mnemonic(mnemonic, 'sr25519'),
+        'account': Cord.Utils.crypto_utils.create_from_mnemonic(mnemonic, 'ed25519'),
         'mnemonic': mnemonic
     }
