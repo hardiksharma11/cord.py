02a3ca0b18d13fd27c479188184ac50fbe44bd1e
Hardik Sharma <hardik182002@gmail.com>
2024-07-16 22:43:38 +0530

    written functions for create_did
    Signed-off-by: Hardik Sharma <hardik182002@gmail.com>

--- packages/config/src/config.py
+++ packages/config/src/config.py
@@ -21,6 +21,7 @@ from substrateinterface.exceptions import SubstrateRequestException
 import asyncio
 from .service import ConfigService
 import logging
+from scalecodec.type_registry import load_type_registry_file
 
 logger = logging.getLogger(__name__)
 
@@ -45,11 +46,14 @@ async def connect(blockchain_rpc_ws_url, no_init_warn=True, **api_options):
     :return: A SubstrateInterface instance.
     """
     try:
+        custom_type = load_type_registry_file('custom-type.json')
         substrate = SubstrateInterface(
             url=blockchain_rpc_ws_url,
-            **api_options
+            **api_options,
+            type_registry=custom_type
         )
-        
+        a = substrate.get_type_definition('pallet_did::did_details::DidCreationDetails<sp_core::crypto::AccountId32, sp_core::crypto::AccountId32, cord_runtime::MaxNewKeyAgreementKeys, pallet_did::service_endpoints::DidEndpoint<T>>')
+        print(a)
         await init({'api': substrate})
         return substrate
     except SubstrateRequestException as e:
