1e1a29724c76b309b6b8d8290f07f92b1231a0bc
Hardik Sharma <hardik182002@gmail.com>
2024-07-24 10:40:56 +0530

    signed
    Signed-off-by: Hardik Sharma <hardik182002@gmail.com>

--- packages/did/src/Did_chain.py
+++ packages/did/src/Did_chain.py
@@ -243,4 +243,4 @@ async def create_did(submitter_account, the_mnemonic = None, did_service_endpoin
 
     print(document)
 
-    return {'mnemonic': mnemonic, 'document': document['document']}
+    return {'mnemonic': mnemonic, 'document': document['document']} 
