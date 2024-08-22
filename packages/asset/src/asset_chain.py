732636c22ea38ceb79e09524a9eca5f21ecaf1be
Hardik Sharma <104981324+hardiksharma11@users.noreply.github.com>
2024-08-22 13:29:23 +0530

    [2] - Asset transfer functions (#26)
    Signed-off-by: Hardik Sharma <hardik182002@gmail.com>

--- packages/asset/src/asset_chain.py
+++ packages/asset/src/asset_chain.py
@@ -4,6 +4,7 @@ from packages.identifier.src.identifier import (
 )
 import json
 from packages.utils.src.SDKErrors import Errors
+from packages.utils.src.prefix import ASSET_PREFIX
 
 async def dispatch_create_to_chain(
     asset_entry,
@@ -94,4 +95,34 @@ async def dispatch_issue_to_chain(
         error_message = str(error)
         raise Errors.CordDispatchError(
             f'Error dispatching to chain: "{error_message}".'
+        )
+
+async def dispatch_transfer_to_chain(
+    asset_entry,
+    author_account,
+    sign_callback
+):
+    try:
+        api = ConfigService.get('api')
+
+        tx = api.compose_call(
+            call_module='Asset',
+            call_function='transfer',
+            call_params={'entry': asset_entry['entry'], 'digest': asset_entry['digest']}
+        )
+
+        extrinsic = await Did.authorize_tx(
+            asset_entry['owner'],
+            tx,
+            sign_callback,
+            author_account.ss58_address
+        )
+        extrinsic = api.create_signed_extrinsic(extrinsic, keypair=author_account)
+        api.submit_extrinsic(extrinsic, wait_for_inclusion=True)
+
+        return f"{ASSET_PREFIX}{asset_entry['entry']['asset_id']}:{asset_entry['entry']['asset_instance_id']}"
+    except Exception as error:
+        error_message = str(error) if isinstance(error, Exception) else json.dumps(error)
+        raise Errors.CordDispatchError(
+            f"Error dispatching to chain: \"{error_message}\"."
         )
